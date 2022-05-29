42cb3dbb8ac9feb7c91d8076e492ae05e0787d1a
Pedro Nasser <nasseraguiarfilho@gmail.com>
2022-05-28 21:42:39 -0600

    Game is pretty much done. The game logic was somewhat hard to figure it out, but at the end I found the solution using something close to a dict from python. Still have some minor events to fix but it's almost enough.

--- game.js
+++ game.js
@@ -1,28 +1,27 @@
 $(function () {
   $("#startButton").on("click", function () {
     validateNicknames();
-    startGame();
   });
 });
 
-var lst1 = {};
-var a1 = 0;
-var a2 = 0;
-var a3 = 0;
+$(".boardSpace").on("click", function () {
+  var locationOnBoard = this.id;
+  gameLogic(locationOnBoard);
+});
 
-var b1 = 0;
-var b2 = 0;
-var b3 = 0;
-
-var c1 = 0;
-var c2 = 0;
-var c3 = 0;
-
-var lst1 = [a1, a2, a3, b1, b2, b3, c1, c2, c3];
+var lst1 = {
+  a1: 0,
+  a2: 0,
+  a3: 0,
+  b1: 0,
+  b2: 0,
+  b3: 0,
+  c1: 0,
+  c2: 0,
+  c3: 0,
+};
 
 var round = 0;
-var p1 = "red";
-var p2 = "blue";
 var currentPlayer = p1; //p1 starts
 var winner = "";
 
@@ -43,48 +42,84 @@ function validateNicknames() {
   $("#p2nick").remove();
 }
 
-function startGame() {
-  //jogo ta rodando sem parar sem esperar o click. Preciso ajeitar isso. Mas por hoje ta bom.
-  while (round < 9 && !conditionToWin()) {
-    play(currentPlayer);
-    round++;
-    if (round < 9 && !conditionToWin()) {
-      currentPlayer = switchPlayer(currentPlayer);
-      play(currentPlayer);
-      round++;
-    }
+function gameLogic(locationOnBoard) {
+  if (round < 9 && !conditionToWin()) {
+    play(locationOnBoard);
   }
-  if (conditionToWin()) winner = currentPlayer;
-  else return false;
-}
-
-function switchPlayer(p) {
-  if (p == "blue") return "red";
-  if (p == "red") return "blue";
+  if (conditionToWin()) {
+    currentPlayer = verifyTurn();
+    alert(currentPlayer + " wins!");
+    return false;
+  }
+  round++;
 }
 
 function conditionToWin() {
-  //horizontal check
-  if (lst1[a1] + lst1[a2] + lst1[a3] == (-3 || 3)) return true;
+  if (
+    lst1["a1"] + lst1["a2"] + lst1["a3"] == -3 ||
+    lst1["a1"] + lst1["a2"] + lst1["a3"] == 3
+  )
+    return true;
 
-  if (lst1[b1] + lst1[b2] + lst1[b3] == (-3 || 3)) return true;
-  if (lst1[c1] + lst1[c2] + lst1[c3] == (-3 || 3)) return true;
+  if (
+    lst1["b1"] + lst1["b2"] + lst1["b3"] == -3 ||
+    lst1["b1"] + lst1["b2"] + lst1["b3"] == 3
+  )
+    return true;
+  if (
+    lst1["c1"] + lst1["c2"] + lst1["c3"] == -3 ||
+    lst1["c1"] + lst1["c2"] + lst1["c3"] == 3
+  )
+    return true;
+
   //vertical check
-  if (lst1[a1] + lst1[b1] + lst1[c1] == (-3 || 3)) return true;
-  if (lst1[a2] + lst1[b2] + lst1[c2] == (-3 || 3)) return true;
-  if (lst1[a3] + lst1[b3] + lst1[c3] == (-3 || 3)) return true;
+  if (
+    lst1["a1"] + lst1["b1"] + lst1["c1"] == -3 ||
+    lst1["a1"] + lst1["b1"] + lst1["c1"] == 3
+  )
+    return true;
+  if (
+    lst1["a2"] + lst1["b2"] + lst1["c2"] == -3 ||
+    lst1["a2"] + lst1["b2"] + lst1["c2"] == 3
+  )
+    return true;
+  if (
+    lst1["a3"] + lst1["b3"] + lst1["c3"] == -3 ||
+    lst1["a3"] + lst1["b3"] + lst1["c3"] == 3
+  )
+    return true;
   //diagonal check
-  if (lst1[a1] + lst1[b2] + lst1[c3] == (-3 || 3)) return true;
-  if (lst1[a3] + lst1[b2] + lst1[c1] == (-3 || 3)) return true;
+  if (
+    lst1["a1"] + lst1["b2"] + lst1["c3"] == -3 ||
+    lst1["a1"] + lst1["b2"] + lst1["c3"] == 3
+  )
+    return true;
+  if (
+    lst1["a3"] + lst1["b2"] + lst1["c1"] == -3 ||
+    lst1["a3"] + lst1["b2"] + lst1["c1"] == 3
+  )
+    return true;
 
   return false;
 }
 
-function play(player) {
-  //player have to choose some option, and then click into a blank space. If its not blank, don't do anything. If its blank
-  //put the respective image on the blank space
-  //x will be assign as -1, and circle will be assign as 1
-  $(".boardSpace").on("click", function () {
-    alert("teste");
-  });
+function play(locationOnBoard) {
+  var icon = "";
+  var score = 0;
+
+  if (round % 2 == 0) {
+    icon = 'url("imagens/marcacao_1.png")';
+    score = -1;
+  } else {
+    icon = 'url("imagens/marcacao_2.png")';
+    score = 1;
+  }
+
+  $("#" + locationOnBoard).css("background-image", icon);
+  lst1[locationOnBoard] = score;
+}
+
+function verifyTurn() {
+  if (round % 2 == 0) return "red";
+  return "blue";
 }
